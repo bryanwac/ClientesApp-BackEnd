bc3b31b0d9aca45a71fc2b8e1ac2b99d0e019549
bryan <bryanwac@gmail.com>
2022-09-23 14:55:10 -0300

    Criadas entidades Servico/Cliente e conns. com DB

---
+++ src/main/java/com/github/bryanwac/clientes/rest/ClienteController.java
@@ -0,0 +1,35 @@
+package com.github.bryanwac.clientes.rest;
+
+import com.github.bryanwac.clientes.model.entity.Cliente;
+import com.github.bryanwac.clientes.model.repository.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+@RestController //Define um controlador com responsebody, tudo que é retornado é um corpo de resposta na requisição
+@RequestMapping("/api/clientes") //Mapeia o URL base a ser tratado no controller
+public class ClienteController {
+
+    private final ClienteRepository repository;
+
+    //Injeção por construtor
+    @Autowired
+    public ClienteController(ClienteRepository repository){
+        this.repository = repository;
+    }
+
+    @PostMapping                         //Mapeando método para requisição Post
+    @ResponseStatus(HttpStatus.CREATED)  //Pegando o Cód.de Status
+    public Cliente salvar (@RequestBody Cliente cliente){
+        return repository.save(cliente); //RequestBody avisando que esse objeto virá no corpo da req.
+    }
+
+                                                         // Define o parametro id como variavel
+    @GetMapping("{id}")                                  // Mapeando para o Get HTTP
+    public Cliente acharPorId(@PathVariable Integer id){ //Define o caminho da variavel e o tipo
+        return repository
+                .findById(id)
+                .orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }// Retorna um Optional, caso encontre, armazena o cliente do ID, caso não, lança um erro de status404
+}
